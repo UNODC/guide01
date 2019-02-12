dad672ec67e649128b37d28d427542c190173084
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2019-02-12 08:40:37 -0500

    added service worker

---
+++ docs/sw.js
@@ -0,0 +1,17 @@
+var CACHE_NAME = "Guide01";
+self.addEventListener('activate', function () {
+    self.clients.claim();
+});
+self.addEventListener('fetch', function (event) {
+    // Response from the cache immediately if possible, but also fetch an update.
+    var cachedOrFetchedResponse = self.caches.open(CACHE_NAME).then(function (cache) {
+        return self.caches.match(event.request).then(function (cacheResponse) {
+            var fetchPromise = self.fetch(event.request).then(function (fetchResponse) {
+                cache.put(event.request, fetchResponse.clone());
+                return fetchResponse;
+            });
+            return cacheResponse || fetchPromise;
+        });
+    });
+    event.respondWith(cachedOrFetchedResponse);
+});
